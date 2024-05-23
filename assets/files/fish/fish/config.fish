534899c716b86ef5508b26358ff9636628b944f2
YvesCousteau <hugohenrotte@hotmail.com>
2024-05-23 16:38:13 +0200

    update fish config

--- assets/files/fish/fish/config.fish
+++ assets/files/fish/fish/config.fish
@@ -1,6 +1,5 @@
-f status is-interactive
+if status is-interactive
   # Commands to run in interactive sessions can go here
   and not set -q TMUX
   exec tmux -u new -A -D -t f4h
 end
-
---
+++ assets/files/fish/fish/functions/wifi.fish
@@ -0,0 +1,4 @@
+function wifi --wraps=nmtui --description 'alias wifi nmtui'
+  nmtui $argv
+        
+end
